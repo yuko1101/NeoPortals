9ae5473534b4745e80bba0947f97a1501c4519ff
yuko1101 <68993883+yuko1101@users.noreply.github.com>
2023-12-15 10:19:59 +0900

    Initial commit

---
+++ src/main/kotlin/io/github/yuko1101/neoportals/NeoPortals.kt
@@ -0,0 +1,15 @@
+package io.github.yuko1101.neoportals
+
+import org.bukkit.plugin.java.JavaPlugin
+
+@Suppress("unused")
+class NeoPortals : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
